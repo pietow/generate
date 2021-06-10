e539a589444f9001fa45ecaf5243018fbabefb44
piet <pprotassow@posteo.de>
2021-06-10 16:37:35 +0200

    css path

--- generate/html5.py
+++ generate/html5.py
@@ -10,7 +10,7 @@ def build_html():
     with doc.head:
         tags.meta(charset='UTF-8')
         tags.meta(name='viewport', content='width=device-width, initial-scale=1.0')
-        link(rel='stylesheet', href='style.css')
+        link(rel='stylesheet', href='css/style.css')
         # script(type='text/javascript', src='script.js')
         with doc:
             with nav(cls='nav').add(ul()):
